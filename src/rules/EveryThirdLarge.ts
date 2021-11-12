3593aa7a214a3d6ae201711136f76b33b90338d6
Arturas Sotnicenko <arturas@reneza.com>
2021-11-12 09:27:30 +0200

    Adds rule for every thiord package for free every month

---
+++ src/rules/EveryThirdLarge.ts
@@ -0,0 +1,31 @@
+import { ShippingRule } from "../decorators";
+import { ShipmentsRepository } from "../repository/Shipments";
+import { Shipment, ShippingCostRule } from "../shared";
+
+/**
+ *  - The third L shipment via LP should be free,
+ *  but only once a calendar month.
+ */
+
+@ShippingRule
+export class EveryThirdLarge extends ShippingCostRule {
+  public applyRule(shipment: Shipment): Shipment {
+    const {
+      shipmentProviderCode,
+      packageSizeCode,
+      shippingCost,
+      shippingDiscount,
+    } = shipment;
+    // probably would be better if incremented somewhere else
+    ShipmentsRepository.incrementMonthlyShipments(shipment);
+    if (
+      shipmentProviderCode === "LP" &&
+      packageSizeCode === "L" &&
+      ShipmentsRepository.monthlyShipmentCount(shipment) === 3
+    ) {
+      shipment.shippingDiscount = shippingDiscount + shippingCost;
+      shipment.shippingCost = 0;
+    }
+    return shipment;
+  }
+}
--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,1 +1,2 @@
+export * from "./EveryThirdLarge";
 export * from "./SmallPackageLowestCostRule";
