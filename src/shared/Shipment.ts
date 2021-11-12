64ac62ee99d1bab61010d0930ae1687b1c21f76c
Arturas Sotnicenko <arturas@reneza.com>
2021-11-12 08:15:27 +0200

    adds validate method for shipment

--- src/shared/Shipment.ts
+++ src/shared/Shipment.ts
@@ -3,23 +3,13 @@ import { ShipmentPricing } from "../repository/ShipmentPricing";
 import { ShippingRulesStorage } from "./ShippingRulesStorage";
 
 export class Shipment {
+  private ignore: boolean = false;
+
   shippingCost: number = Infinity;
   shippingDiscount: number = 0;
   date!: Date;
   shipmentProviderCode!: ShippingProvider;
   packageSizeCode!: PackingSize;
-
-  public fromString(str: string, delimiter = " "): Shipment {
-    const [date, packageSizeCode, shipmentProviderCode] = str.split(delimiter);
-    this.date = new Date(date);
-    this.shipmentProviderCode = shipmentProviderCode as ShippingProvider;
-    this.packageSizeCode = packageSizeCode as PackingSize;
-    return this;
-  }
-
-  public toString(delimiter = " "): string {
-    return `${this.date}${delimiter}${this.shipmentProviderCode}${delimiter}${this.packageSizeCode}${delimiter}${this.shippingCost}${delimiter}${this.shippingDiscount} \n`;
-  }
 
   public applyShippingRates() {
     this.shippingCost = new ShipmentPricing().getPrice(this);
@@ -27,6 +17,7 @@ export class Shipment {
 
   public applyShippingRules() {
     let shippingRules = ShippingRulesStorage.getRules() as Map<any, any>;
+
     shippingRules.forEach((shippingRule, key) => {
       const shipmentWithRulesApplied = shippingRule.applyRule(this);
       Object.assign(this, shipmentWithRulesApplied);
@@ -36,5 +27,30 @@ export class Shipment {
       // this.shipmentProviderCode = shipmentWithRulesApplied.shipmentProviderCode;
       // this.packageSizeCode = shipmentWithRulesApplied.packageSizeCode;
     });
+  }
+
+  public validate(): boolean {
+    if (
+      this.date === undefined ||
+      this.shipmentProviderCode === undefined ||
+      this.packageSizeCode === undefined
+    ) {
+      this.ignore = true;
+      return false;
+    }
+    return true;
+  }
+
+  public fromString(str: string, delimiter = " "): Shipment {
+    const [date, packageSizeCode, shipmentProviderCode] = str.split(delimiter);
+
+    this.date = new Date(date);
+    this.shipmentProviderCode = shipmentProviderCode as ShippingProvider;
+    this.packageSizeCode = packageSizeCode as PackingSize;
+    return this;
+  }
+
+  public toString(delimiter = " "): string {
+    return `${this.date}${delimiter}${this.shipmentProviderCode}${delimiter}${this.packageSizeCode}${delimiter}${this.shippingCost}${delimiter}${this.shippingDiscount} \n`;
   }
 }
