f9e25e37e4c8b4f9bd2983a63677482ffbd834ca
Arturas Sotnicenko <arturas@reneza.com>
2021-11-13 08:11:05 +0200

    Changes Main class - adds parameters to constructor, pocess input now returns a promise, moves output event and output stream hadnling to seperate method (for testing purposese)

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,55 @@
 import { createReadStream } from "fs";
 import { Shipment } from "./shared/Shipment";
 import "./rules";
+import { EventEmitter } from "stream";
 
-export class Main {
+export class Main extends EventEmitter {
   private filename: string;
-  constructor(filename = "input.txt") {
+  private lineEndings: string | RegExp;
+  private delimiter: string;
+  constructor(
+    {
+      filename,
+      lineEndings,
+      delimiter,
+    }: { filename: string; lineEndings: string | RegExp; delimiter: string } = {
+      filename: "input.txt",
+      lineEndings: /\r?\n/,
+      delimiter: " ",
+    }
+  ) {
+    super();
     this.filename = filename;
-    this.processInput();
+    this.lineEndings = lineEndings;
+    this.delimiter = delimiter;
+
+    this.on("shipmentData", (data) => {
+      this.output(data.toString());
+    });
   }
 
-  //create read stream to read input.txt file line by line and process each line
-  public processInput() {
-    const inputStream = createReadStream(this.filename);
-    inputStream.on("data", (chunk) => {
-      const lines = chunk.toString().split(/\r?\n/);
-      lines.forEach((line) => {
-        const shipment = new Shipment().fromString(line);
-        if (shipment.validate()) {
-          shipment.applyShippingRates();
-          shipment.applyShippingRules();
-        }
-        process.stdout.write(shipment.toString());
+  public output(data: string) {
+    process.stdout.write(data.toString());
+  }
+
+  public async processInput() {
+    return new Promise((resolve, reject) => {
+      const inputStream = createReadStream(this.filename);
+
+      inputStream.on("data", (chunk) => {
+        const lines = chunk.toString().split(this.lineEndings);
+        lines.forEach((line) => {
+          const shipment = new Shipment().fromString(line, this.delimiter);
+          if (shipment.validate()) {
+            shipment.applyShippingRates();
+            shipment.applyShippingRules();
+          }
+          this.emit("shipmentData", shipment.toString());
+        });
+      });
+
+      inputStream.on("end", () => {
+        resolve(true);
       });
     });
   }
