be27518fd546c338cef895623b64ed3204bb2554
Arturas Sotnicenko <arturas@reneza.com>
2021-11-15 20:10:40 +0200

    adds delimiter param to vint init function

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,7 @@ import { VintShippingRules } from "./VintShippingRules";
 
 const vintShippingRules = new VintShippingRules({
   pathToFile: process.argv[2] || "input.txt",
+  delimiter: process.argv[3] || " ",
 });
 
 (async () => {
